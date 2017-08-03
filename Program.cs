54c551a4a798172f91f06ff6f336f7850f7e4223
Taylor Southwick <tasou@microsoft.com>
2017-08-03 07:22:40 -0700

    Initial repro

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Packaging;
+using System.Windows;
+using DocumentFormat.OpenXml.Packaging;
+using System.IO;
+using System.Reflection;
+
+namespace TestOpenXML
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string fileName = @"../../task.xlsx";
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, true))
+			{
+				Console.WriteLine();
+				Console.ReadKey();
+			}
+
+
+        }
+    }
+}
